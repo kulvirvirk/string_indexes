6313493dbbb1e8e5a26ce9a243d601d5e715c00e
kulvirvirk <kvirkdev@gmail.com>
2020-02-12 23:54:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# 1. create a string 
+# 2. grab different elements from various index values 
+# 3. test slice functionality 
+# 4. test stride with slice
+
+
+str1 = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+
+exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+
+sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
+#print the full string 
+print(str1)
+
