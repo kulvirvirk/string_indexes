946e31e4d67489d6f31e0820efcdf48dc8d10bc6
kulvirvirk <kvirkdev@gmail.com>
2020-02-13 00:05:16 +0000

    added comments, and stride/step over code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # 2. grab different elements from various index values 
 # 3. test slice functionality 
 # 4. test stride with slice
-
+# 5. test step or stride with string slicing
 
 str1 = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
 
@@ -10,9 +10,15 @@ exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
 
 sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
 #print the full string 
-print(str1)
+print(str1 + '\n')
 
+#printing string's index[44]
+print (str1[44]+ '\n')
 
-print (str1[44])
+#printing string's chars form range 44 to 55
+print(str1[44:55] + '\n')
+print(len(str1))
 
-print(str1[44:55])
+#printing string's chars from range 5 to 444 but also stepping over by 2
+print('=========\n' + str1[5:444:2]+ '\n')
+print('=========\n')
